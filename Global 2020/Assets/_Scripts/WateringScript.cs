4c7ca8c3d6fe70bdb0ab74c7f6cd8dab33218146
EmFresh <emory.wynn@uoit.net>
2020-02-01 13:09:16 -0500

    Watering can

---
+++ Global 2020/Assets/_Scripts/WateringScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WateringScript : MonoBehaviour
+{
+    public ParticleSystem system;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.rotation.x > 45)
+        
+            system.Play();
+        else
+            system.Stop();
+    }
+}
