693d9f2d3f54a611f4e33d0e21be07eca4088904
Rob <robert.andersen@ontariotechu.net>
2020-02-01 15:28:08 -0500

    Button works , working on box

---
+++ Global 2020/Assets/_Scripts/CardboardBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardboardBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetChild(0).tag == "Plant")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
