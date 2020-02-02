8c4f1aee30bea82b58795f9303de875026dd9045
Alex Zettler[Desktop] <azettler@live.com>
2020-02-02 10:37:56 -0500

    Now with comments

--- Global 2020/Assets/_Scripts/PlantFactory.cs
+++ Global 2020/Assets/_Scripts/PlantFactory.cs
@@ -12,6 +12,9 @@ public class PlantFactory : MonoBehaviour
 
     public Transform spawnOrientation;
 
+    public bool CreateFlower;
+
+    //The prefabs required to generate a plant bottom
     [System.Serializable]
     public class PlantBottom
     {
@@ -27,6 +30,7 @@ public class PlantFactory : MonoBehaviour
         public GameObject soilNeedsFertalizer;
     }
 
+    //The prefabs required to generate a plant stem
     [System.Serializable]
     public class PlantStem
     {
@@ -37,6 +41,7 @@ public class PlantFactory : MonoBehaviour
         public GameObject stemBroken;
     }
 
+    //The prefabs required to generate a plant head
     [System.Serializable]
     public class PlantHead
     {
@@ -46,11 +51,12 @@ public class PlantFactory : MonoBehaviour
         public GameObject headPetalMissing;
     }
 
-
+    //The list of each plant components that can be used to generate a plant
     public List<PlantBottom> plantBottoms;
     public List<PlantStem> plantStems;
     public List<PlantHead> plantHeads;
 
+    //The PoolEnumListStrLookup key options to build the ObjectPool tag lookup from
     public enum PoolObjects
     {
         pot_normal,
@@ -68,7 +74,117 @@ public class PlantFactory : MonoBehaviour
         head_petal_missing
     }
 
+    // A PoolObject to list of possilble tags to retrieve from the ObjectPool
     public Dictionary<PoolObjects, List<string>> PoolEnumListStrLookup = new Dictionary<PoolObjects, List<string>>();
+
+
+    // Adds a GameObject to the OjectPool and PoolEnumListStrLookup dictionary
+    private void addToStructures(string tag_prefix, string tag_suffix, PoolObjects pool_enum_val, GameObject prefab, int pool_instances)
+    {
+        string tag = tag_prefix + tag_suffix;
+        myPool.AddPool(new ObjectPool.Pool(tag, prefab, pool_instances));
+
+        //Debug.Log(PoolEnumListStrLookup[pool_enum_val]);
+
+        if (PoolEnumListStrLookup.ContainsKey(pool_enum_val))
+        {
+            PoolEnumListStrLookup[pool_enum_val].Add(tag);
+        }
+        else
+        {
+            PoolEnumListStrLookup.Add(pool_enum_val, new List<string>() { tag });
+        }
+
+    }
+
+    public enum HeadState
+    {
+        normal,
+        missing_petals,
+    }
+    //Maps the head_state enumeate to a PoolObject enumerate
+    public Dictionary<HeadState, PoolObjects> HeadStatePoolObjectMapping = new Dictionary<HeadState, PoolObjects>()
+    {
+            {HeadState.normal, PoolObjects.head_normal},
+            {HeadState.missing_petals, PoolObjects.head_petal_missing},
+    };
+        
+    
+    public enum StemState
+    {
+        normal,
+        limp,
+        broken,
+    }
+    //Maps the stem_state enumeate to a PoolObject enumerate
+    public Dictionary<StemState, PoolObjects> StemStatePoolObjectMapping = new Dictionary<StemState, PoolObjects>()
+    {
+            {StemState.normal, PoolObjects.stem_normal},
+            {StemState.limp, PoolObjects.stem_limp},
+            {StemState.broken, PoolObjects.stem_broken},
+    };
+
+    public enum PotState
+    {
+        normal,
+        broken,
+    }
+    //Maps the pot_state enumeate to a PoolObject enumerate
+    public Dictionary<PotState, PoolObjects> PotStatePoolObjectMapping = new Dictionary<PotState, PoolObjects>()
+    {
+            {PotState.normal, PoolObjects.pot_normal},
+            {PotState.broken, PoolObjects.pot_broken},
+    };
+
+    public enum SoilState
+    {
+        normal,
+        dry,
+        discolored,
+    }
+    //Maps the soil_state enumeate to a PoolObject enumerate
+    public Dictionary<SoilState, PoolObjects> SoilStatePoolObjectMapping = new Dictionary<SoilState, PoolObjects>()
+    {
+            {SoilState.normal, PoolObjects.soil_normal},
+            {SoilState.dry, PoolObjects.soil_dry},
+            {SoilState.discolored, PoolObjects.soil_needs_fertilizer},
+    };
+
+
+    //An enum containing all possible problems with a plant
+    public enum PlantProblem
+    {
+        thirsty,
+        broken_pot,
+        bumped,
+        plant_too_large,
+        malnourished,
+        infestation,
+        sick,
+        romantic_owner,
+    }
+
+
+    //Select a random enumerate value
+    static T RandomEnumValue<T>()
+    {
+        var v = Enum.GetValues(typeof(T));
+        return (T)v.GetValue(new System.Random().Next(v.Length));
+    }
+
+    //Select a random list item
+    static T RandomListElement<T>(List<T> list)
+    {
+        int index = (new System.Random()).Next(list.Count);
+        return list[index];
+    }
+
+    //Select a random problem for a plant
+    private PlantProblem GenerateProblem()
+    {
+        return RandomEnumValue<PlantProblem>();
+    }
+
 
 
     // Start is called before the first frame update
@@ -78,7 +194,8 @@ public class PlantFactory : MonoBehaviour
 
         int instances = 20;
 
-        foreach (PlantBottom pb in plantBottoms) {
+        foreach (PlantBottom pb in plantBottoms)
+        {
             addToStructures(pb.tag, "_pot_normal", PoolObjects.pot_normal, pb.potNormal, instances);
             addToStructures(pb.tag, "_pot_broken", PoolObjects.pot_broken, pb.potBroken, instances);
 
@@ -103,7 +220,7 @@ public class PlantFactory : MonoBehaviour
         }
     }
 
-    public bool CreateFlower;
+    
     private void Update()
     {
         if (CreateFlower)
@@ -111,107 +228,10 @@ public class PlantFactory : MonoBehaviour
             CreatePlant();
             CreateFlower = false;
         }
-        
-    }
-
-    private void addToStructures(string tag_prefix, string tag_suffix, PoolObjects pool_enum_val, GameObject prefab, int pool_instances)
-    {
-        string tag = tag_prefix + tag_suffix;
-        myPool.AddPool(new ObjectPool.Pool(tag, prefab, pool_instances));
-
-        //Debug.Log(PoolEnumListStrLookup[pool_enum_val]);
-
-        if (PoolEnumListStrLookup.ContainsKey(pool_enum_val))
-        {
-            PoolEnumListStrLookup[pool_enum_val].Add(tag);
-        }
-        else
-        {
-            PoolEnumListStrLookup.Add(pool_enum_val, new List<string>() { tag });
-        }
 
     }
 
-    public enum HeadState
-    {
-        normal,
-        missing_petals,
-    }
-    public Dictionary<HeadState, PoolObjects> HeadStatePoolObjectMapping = new Dictionary<HeadState, PoolObjects>()
-    {
-            {HeadState.normal, PoolObjects.head_normal},
-            {HeadState.missing_petals, PoolObjects.head_petal_missing},
-    };
-        
-
-    public enum StemState
-    {
-        normal,
-        limp,
-        broken,
-    }
-    public Dictionary<StemState, PoolObjects> StemStatePoolObjectMapping = new Dictionary<StemState, PoolObjects>()
-    {
-            {StemState.normal, PoolObjects.stem_normal},
-            {StemState.limp, PoolObjects.stem_limp},
-            {StemState.broken, PoolObjects.stem_broken},
-    };
-    public enum PotState
-    {
-        normal,
-        broken,
-    }
-    public Dictionary<PotState, PoolObjects> PotStatePoolObjectMapping = new Dictionary<PotState, PoolObjects>()
-    {
-            {PotState.normal, PoolObjects.pot_normal},
-            {PotState.broken, PoolObjects.pot_broken},
-    };
-
-    public enum SoilState
-    {
-        normal,
-        dry,
-        discolored,
-    }
-    public Dictionary<SoilState, PoolObjects> SoilStatePoolObjectMapping = new Dictionary<SoilState, PoolObjects>()
-    {
-            {SoilState.normal, PoolObjects.soil_normal},
-            {SoilState.dry, PoolObjects.soil_dry},
-            {SoilState.discolored, PoolObjects.soil_needs_fertilizer},
-    };
-
-    public enum PlantProblem
-    {
-        thirsty,
-        broken_pot,
-        bumped,
-        plant_too_large,
-        malnourished,
-        infestation,
-        sick,
-        romantic_owner,
-    }
-
-
-
-    static T RandomEnumValue<T>()
-    {
-        var v = Enum.GetValues(typeof(T));
-        return (T)v.GetValue(new System.Random().Next(v.Length));
-    }
-
-    static T RandomListElement<T>(List<T> list)
-    {
-        int index = (new System.Random()).Next(list.Count);
-        return list[index];
-    }
-
-    private PlantProblem GenerateProblem()
-    {
-        return RandomEnumValue<PlantProblem>();
-    }
-
-
+    //Builds the things wrong with a plant from a plant problem
     private Tuple<HeadState, StemState, PotState, SoilState> AttributeBuilder(PlantProblem problem)
     {
         //Tuple<HeadState, StemState, PotState, SoilState> plant_state;
@@ -258,6 +278,8 @@ public class PlantFactory : MonoBehaviour
 
 
 
+
+
     public GameObject CreatePlant()
     {
         PlantProblem problem = GenerateProblem();
@@ -271,30 +293,43 @@ public class PlantFactory : MonoBehaviour
         SoilState esoil = attributes.Item4;
 
 
-
+        //Gets the 
         string head_tag = RandomListElement<string>(PoolEnumListStrLookup[HeadStatePoolObjectMapping[ehead]]);
         string stem_tag = RandomListElement<string>(PoolEnumListStrLookup[StemStatePoolObjectMapping[estem]]);
         string pot_tag = RandomListElement<string>(PoolEnumListStrLookup[PotStatePoolObjectMapping[epot]]);
         string soil_tag = RandomListElement<string>(PoolEnumListStrLookup[SoilStatePoolObjectMapping[esoil]]);
 
-        GameObject plant_parent = new GameObject();
-        plant_parent.name = "PlantParent";
+        GameObject plant_parent = new GameObject
+        {
+            name = "PlantParent"
+        };
         plant_parent.transform.position = spawnOrientation.position;
         Debug.Log(plant_parent.transform.position);
         plant_parent.transform.rotation = spawnOrientation.rotation;
         plant_parent.AddComponent<Rigidbody>();
 
-        GameObject plant_top = new GameObject();
-        plant_top.name = "PlantTop";
+        GameObject plant_top = new GameObject
+        {
+            name = "PlantTop"
+        };
         plant_top.transform.parent = plant_parent.transform;
 
         GameObject plant_stem = myPool.SpawnObject(stem_tag, new Vector3(0,0,0), new Quaternion(), plant_top.transform);
+
+        // TODO: ability to lookup a child gameobject that contains the transform pos and rot and add the head to it.
+        //GameObject stem_head_parent = new GameObject();
+        //stem_head_parent.name = "head_location";
+
         GameObject plant_head = myPool.SpawnObject(head_tag, new Vector3(0, 0, 0), new Quaternion(), plant_stem.transform);
         GameObject plant_soil = myPool.SpawnObject(soil_tag, new Vector3(0, 0, 0), new Quaternion(), plant_top.transform);
 
 
-        GameObject plant_bottom = new GameObject();
-        plant_bottom.name = "PlantBottom";
+
+        
+        GameObject plant_bottom = new GameObject
+        {
+            name = "PlantBottom"
+        };
         plant_bottom.transform.parent = plant_parent.transform;
 
         GameObject plant_pot = myPool.SpawnObject(pot_tag, new Vector3(0, 0, 0), new Quaternion(), plant_bottom.transform);
