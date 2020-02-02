9e4658207086fb85c9740d7de3b1c7f3df1c1902
EmFresh <emory.wynn@uoit.net>
2020-02-01 19:25:23 -0500

    Maybe fixed Can?

--- Global 2020/Assets/_Scripts/WateringScript.cs
+++ Global 2020/Assets/_Scripts/WateringScript.cs
@@ -9,10 +9,22 @@ public class WateringScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.rotation.x < -20)
-        
+        Vector3 forward = multiVec3(transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 vecDown = transform.forward;
+
+        float angle = Mathf.Acos(Dot(forward, vecDown) / (forward.magnitude * vecDown.magnitude)) * 180 / Mathf.PI;
+
+        if (angle > 25 && vecDown.y < 0)
+
             system.Play();
         else
             system.Stop();
+    }
+
+    private Vector3 multiVec3(Vector3 a, Vector3 b) => new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
+
+    float Dot(Vector3 a, Vector3 b)
+    {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
     }
 }
