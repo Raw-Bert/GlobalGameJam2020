3289fa63e3d5a35868a332d3dec5205c1fe3c32d
Lillian <ziqi.fan@ontariotech.net>
2020-02-01 22:43:26 -0500

    Main Menu works with Pointer

---
+++ Global 2020/Assets/_Scripts/UIButtonTransition.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+// Reference: https://www.youtube.com/watch?v=vNqHRD4sqPc&list=PLmc6GPFDyfw85CcfwbB7ptNVJn5BSBaXz&index=3
+
+public class UIButtonTransition : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+{
+    public Color32 normalColor = Color.white;
+    public Color32 hoverColor = Color.grey;
+    public Color32 downColor = Color.blue;
+
+    private Image m_image = null;
+
+    bool mouseOver = false;
+    bool mouseOverExit = false;
+    Vector3 minScale;
+    Vector3 currentScale;
+    public Vector3 targetScale;
+    public Vector3 maxScale;
+
+    public GameObject credit = null;
+
+    private void Awake()
+    {
+        m_image = GetComponent<Image>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_image.color = hoverColor;
+
+        mouseOver = true;
+        mouseOverExit = false;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_image.color = normalColor;
+
+        mouseOver = false;
+        mouseOverExit = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(gameObject.tag == "Start")
+        {
+            SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
+        }
+        if (gameObject.tag == "Credit")
+        {
+            spawnCredit();
+        }
+        if (gameObject.tag == "Exit")
+        {
+            Application.Quit();
+        }
+
+        m_image.color = downColor;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        m_image.color = hoverColor;
+    }
+
+    void Update()
+    {
+        if (mouseOver)
+        {
+            minScale = transform.localScale;
+            transform.localScale = Vector2.Lerp(minScale, maxScale, .25f);
+        }
+
+        if (mouseOverExit)
+        {
+            currentScale = transform.localScale;
+            transform.localScale = Vector2.Lerp(currentScale, targetScale, .25f);
+        }
+    }
+
+    private void spawnCredit()
+    {
+        Instantiate(credit, transform.position, Quaternion.identity);
+    }
+}
---
+++ Global 2020/Assets/_Scripts/VRInput.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+// Reference: https://www.youtube.com/watch?v=vNqHRD4sqPc&list=PLmc6GPFDyfw85CcfwbB7ptNVJn5BSBaXz&index=3
+
+public class VRInput : BaseInputModule
+{
+    public Camera m_camera;
+    public SteamVR_Input_Sources targetSource;
+    public SteamVR_Action_Boolean clickAction;
+
+    private GameObject currentObject = null;
+    private PointerEventData data = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        data = new PointerEventData(eventSystem);
+    }
+
+    public override void Process()
+    {
+        // Reset data, set camera
+        data.Reset();
+        data.position = new Vector2(m_camera.pixelWidth / 2, m_camera.pixelHeight / 2);
+
+        // Raycast
+        eventSystem.RaycastAll(data, m_RaycastResultCache);
+        data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        currentObject = data.pointerCurrentRaycast.gameObject;
+
+        // Clear
+        m_RaycastResultCache.Clear();
+
+        // Hover
+        HandlePointerExitAndEnter(data, currentObject);
+
+        // Press
+        if (clickAction.GetStateDown(targetSource))
+            ProcessPress(data);
+
+        // Release
+        if (clickAction.GetStateUp(targetSource))
+            ProcessRelease(data);
+
+    }
+
+    public PointerEventData GetData()
+    {
+        return data;
+    }
+
+    private void ProcessPress(PointerEventData dat)
+    {
+        // Set raycast
+        dat.pointerPressRaycast = dat.pointerCurrentRaycast;
+
+        // Check for hit, get down handler, call
+        GameObject newPointerPress = ExecuteEvents.ExecuteHierarchy(currentObject, dat, ExecuteEvents.pointerDownHandler);
+
+        // If no down handler, try get click
+        if (newPointerPress = null)
+            newPointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentObject);
+
+        // Set data
+        dat.pressPosition = dat.position;
+        dat.pointerPress = newPointerPress;
+        dat.rawPointerPress = currentObject;
+    }
+
+    private void ProcessRelease(PointerEventData dat)
+    {
+        // Pointer up
+        ExecuteEvents.Execute(dat.pointerPress, dat, ExecuteEvents.pointerUpHandler);
+
+        // Check cilck handler
+        GameObject pointerUpHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentObject);
+
+        // Check if actual
+        if(dat.pointerPress == pointerUpHandler)
+        {
+            ExecuteEvents.Execute(dat.pointerPress, dat, ExecuteEvents.pointerClickHandler);
+        }
+
+        // Clear
+        eventSystem.SetSelectedGameObject(null);
+
+        // Reset data
+        dat.pressPosition = Vector2.zero;
+        dat.pointerPress = null;
+        dat.rawPointerPress = null;
+    }
+}
