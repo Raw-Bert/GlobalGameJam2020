5ec34e2fd99ac9829937dfc8c82a36f89faefa46
Rob <robert.andersen@ontariotechu.net>
2020-02-02 09:09:20 -0500

    Working?

--- Global 2020/Assets/_Scripts/WateringScript.cs
+++ Global 2020/Assets/_Scripts/WateringScript.cs
@@ -6,19 +6,35 @@ public class WateringScript : MonoBehaviour
 {
     public ParticleSystem system;
 
+    void Start()
+    {
+
+        system = Instantiate(system);
+
+        system.gameObject.transform.parent = transform; 
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 forward = multiVec3(transform.forward, new Vector3(1, 0, 1)).normalized;
-        Vector3 vecDown = transform.forward;
+        Vector3 vecDown = transform.forward.normalized;
 
         float angle = Mathf.Acos(Dot(forward, vecDown) / (forward.magnitude * vecDown.magnitude)) * 180 / Mathf.PI;
 
-        if (angle > 25 && vecDown.y < 0)
+        if (angle > 25.0f && vecDown.y < 0.0f)
+        {
+            system.gameObject.SetActive(true);
 
-            system.Play();
+        }
+
         else
-            system.Stop();
+            system.gameObject.SetActive(false);
+
+
+        system.transform.position = transform.position;
+
+
     }
 
     private Vector3 multiVec3(Vector3 a, Vector3 b) => new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
